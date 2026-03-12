4e574abe60bf08af2cba06d05a90c12161115265
rory_black <rory.blackman04@gmail.com>
2026-03-12 12:20:25 +0000

    code refactorings and precision metric added to ZeDPEB

--- ZeDPEB.py
+++ ZeDPEB.py
@@ -11,9 +11,14 @@ import time
 import json
 import requests
 from bs4 import BeautifulSoup
+from enum import Enum
 
 from DISARM_DATA_MASTER import DISARMDataMaster
 from DISARM_interpreter import DISARMClassifier, get_mitre_external_id
+
+class ClfMode(Enum):
+    SELECT_ALL = "SelectAll"
+    BATCH = "Batch"
 
 def get_related_tactic(technique_external_id):
         with open("DISARM.json", "r", encoding="utf-8") as f:
@@ -78,23 +83,34 @@ def partial_match(correct_technique, identified_techniques):
             return True
     return False
 
+def precision(n_pos, n_false):
+    try:
+        p = n_pos / (n_pos + n_false)
+    except ZeroDivisionError:
+        return 0
+    return p
+
+
 # Todo: implement precision statistics
-def test_batch_clf(num_tests=-1, checkpoint=0):
+def test_clf(mode=ClfMode.BATCH,num_tests=-1, checkpoint=0):
+    is_batch = mode == ClfMode.BATCH
+
     start_time = time.time()
     disarm_data = DISARMDataMaster()
     new_log_file()
     incident_ids = disarm_data.get_incident_ids()
     # variables to keep track of overall performance
-    ta_total = t_total = ta_mtchs_total = t_abs_mtchs_total = t_prt_mtchs_total = 0
+    ta_total = t_total = ta_mtchs_total = t_mtchs_total = t_abs_mtchs_total = t_prt_mtchs_total = 0
+    ta_false_total = t_false_total = t_abs_false_total = 0
     
     # incident loop
     for incident_id in incident_ids:
         # break loop if number of tests reached
         if num_tests == 0:
             break
-        tactic_matches = 0
-        technique_matches = 0
-        technique_partial_matches = 0
+        tactic_positives = 0
+        technique_abs_positives = 0
+        technique_partial_positives = 0
 
         # get the incident data
         result = retrieve_incident_data(disarm_data, incident_id)
@@ -112,117 +128,81 @@ def test_batch_clf(num_tests=-1, checkpoint=0):
         t_total += len(article_techniques)
         
         disarm_classifer = DISARMClassifier()
-        print_log(f"Testing for incident with ID: {incident_id}\nTactics: {article_tactics}\nTechniques: {article_techniques}\n")
+        print_log(f"Testing for incident with ID: {incident_id} \n{f"Tactics: {article_tactics}\n" if is_batch else ""}Techniques: {article_techniques}\n")
         print_log(f"Classifier log created in: {disarm_classifer.log_filename}\n")
         print_log(f"Article Character Length: {len(article_content)}\n")
 
-        # Perform Tactic classifications:
-        identified_tactics = disarm_classifer.identify_tactics(article_content)
-        # reset chat history for techniques (to emulate normal execution of batchCLF loop)
-        disarm_classifer.conversation_history_main = []
         identified_techniques = []
-        print(f"Tactics identified by model: {identified_tactics}")
-        for required_tactic in article_tactics:
-            if required_tactic in identified_tactics:
-                tactic_matches+=1
-            # Perform nested technique classifications (ONLY for relevant tactics, no need to do full batchClf):
-            identified_techniques += disarm_classifer.identify_techniques_for_tactic(required_tactic, article_content)
-        print(f"Techniques identified by model: {identified_techniques}")
-        for required_technique in article_techniques:
-            if required_technique in identified_techniques:
-                technique_matches+=1
-            elif partial_match(required_technique, identified_techniques):
-                technique_partial_matches+=1
-        # record results
-        ta_mtchs_total += tactic_matches
-        t_abs_mtchs_total += technique_matches
-        t_prt_mtchs_total += technique_partial_matches
-        results_log = f"""Results:
-Identified tactics({len(identified_tactics)}): {identified_tactics}
-Identified techniques({(len(identified_techniques))}): {identified_techniques}
+        identified_tactics = []
 
-Correct Tactics: {tactic_matches}/{len(article_tactics)}
-Correct Techniques: {technique_matches + technique_partial_matches}/{len(article_techniques)}
-    Absolute technique matches: {technique_matches}
-    Partial technique matches: {technique_partial_matches}
-{'-'*50}"""
-        print_log(results_log)
-        num_tests-=1
-    final_log = f"""
-{'='*50}
-Final Results:
-{'-'*50}
-Correct Tactics: {ta_mtchs_total}/{ta_total} \t ({(ta_mtchs_total/ta_total)*100}%)
-Correct Techniques: {t_abs_mtchs_total + t_prt_mtchs_total}/{t_total} \t ({((t_abs_mtchs_total+t_prt_mtchs_total)/t_total)*100}%)
-    Absolute technique matches: {t_abs_mtchs_total} \t ({(t_abs_mtchs_total/t_total)*100}%)
-    Partial technique matches: {t_prt_mtchs_total} \t ({(t_prt_mtchs_total/t_total)*100}%)
-Time Taken: {round((time.time() - start_time)/60, 2)} minuites
-{'='*50}"""
-    print_log(final_log)
-
-def test_single_clf(num_tests=-1, checkpoint=0):
-    start_time = time.time()
-    disarm_data = DISARMDataMaster()
-    new_log_file()
-    incident_ids = disarm_data.get_incident_ids()
-    # variables to keep track of overall performance
-    t_total = ta_mtchs_total = t_abs_mtchs_total = t_prt_mtchs_total = 0
-    
-    # incident loop
-    for incident_id in incident_ids:
-        # break loop if number of tests reached
-        if num_tests == 0:
-            break
-        technique_matches = 0
-        technique_partial_matches = 0
-
-        # get the incident data
-        result = retrieve_incident_data(disarm_data, incident_id)
-
-        # skip if no url/data is available
-        if result is None:
-            print_log(f"Skipping incident {incident_id} - missing data\n")
-            continue
-        if checkpoint > 0:
-            print_log(f"Skipping incident {incident_id} by user request\n")
-            checkpoint -= 1
-            continue
-        article_content, article_tactics, article_techniques = result
-        t_total += len(article_techniques)
+        if is_batch:
+            # Perform Tactic classifications:
+            identified_tactics = disarm_classifer.identify_tactics(article_content)
+            # reset chat history for techniques (to emulate normal execution of batchCLF loop)
+            disarm_classifer.conversation_history_main = []
         
-        disarm_classifer = DISARMClassifier()
-        print_log(f"Testing for incident with ID: {incident_id}\nTechniques: {article_techniques}\n")
-        print_log(f"Classifier log created in: {disarm_classifer.log_filename}\n")
-        print_log(f"Article Character Length: {len(article_content)}\n")
+            print(f"Tactics identified by model: {identified_tactics}")
+            for required_tactic in article_tactics:
+                if required_tactic in identified_tactics:
+                    tactic_positives+=1
+                # Perform nested technique classifications (ONLY for relevant tactics, no need to do full batchClf):
+                identified_techniques += disarm_classifer.identify_techniques_for_tactic(required_tactic, article_content)
+        elif mode == ClfMode.SELECT_ALL:
+            identified_techniques = disarm_classifer.identify_techniques(article_content)
 
-        # perform technique classification for all techniques
-        identified_techniques = disarm_classifer.identify_techniques(article_content)
         print(f"Techniques identified by model: {identified_techniques}")
         for required_technique in article_techniques:
             if required_technique in identified_techniques:
-                technique_matches+=1
+                technique_abs_positives+=1
             elif partial_match(required_technique, identified_techniques):
-                technique_partial_matches+=1
+                technique_partial_positives+=1
         # record results
-        t_abs_mtchs_total += technique_matches
-        t_prt_mtchs_total += technique_partial_matches
+        technique_positives = technique_abs_positives + technique_partial_positives
+        ta_false = len(identified_tactics) - tactic_positives
+        t_false = len(identified_techniques) - (technique_positives)
+        t_abs_false = len(identified_techniques) - (technique_abs_positives)
+        # update totals
+        ta_mtchs_total += tactic_positives
+        t_mtchs_total += technique_positives
+        t_abs_mtchs_total += technique_abs_positives
+        t_prt_mtchs_total += technique_partial_positives
+
+        ta_false_total += ta_false
+        t_false_total += t_false
+        t_abs_false_total += t_abs_false
+
         results_log = f"""Results:
+{f"Identified tactics({len(identified_tactics)}): {identified_tactics}" if is_batch else ""}
 Identified techniques({(len(identified_techniques))}): {identified_techniques}
 
-Correct Techniques: {technique_matches + technique_partial_matches}/{len(article_techniques)}
-    Absolute technique matches: {technique_matches}
-    Partial technique matches: {technique_partial_matches}
+Recall:
+{f"Correct Tactics: {tactic_positives}/{len(article_tactics)}" if is_batch else ""}
+Correct Techniques: {technique_positives}/{len(article_techniques)}
+    Absolute technique matches: {technique_abs_positives}
+    Partial technique matches: {technique_partial_positives}
+
+Precision:
+{f"Tactics: {precision(tactic_positives, ta_false)}" if is_batch else ""}
+Techniques: {precision(technique_positives, t_false)}
+    Absolute Techniques: {precision(technique_abs_positives, t_abs_false)}
 {'-'*50}"""
         print_log(results_log)
         num_tests-=1
-
     final_log = f"""
 {'='*50}
 Final Results:
 {'-'*50}
-Correct Techniques: {t_abs_mtchs_total + t_prt_mtchs_total}/{t_total} \t ({((t_abs_mtchs_total+t_prt_mtchs_total)/t_total)*100}%)
+Recall:
+{f"Correct Tactics: {ta_mtchs_total}/{ta_total} \t ({(ta_mtchs_total/ta_total)*100}%)" if is_batch else ""}
+Correct Techniques: {t_mtchs_total}/{t_total} \t ({((t_mtchs_total)/t_total)*100}%)
     Absolute technique matches: {t_abs_mtchs_total} \t ({(t_abs_mtchs_total/t_total)*100}%)
     Partial technique matches: {t_prt_mtchs_total} \t ({(t_prt_mtchs_total/t_total)*100}%)
+{'-'*50}
+Precision:
+{f"Tactics: {precision(ta_mtchs_total, ta_false_total)}" if is_batch else ""}
+Techniques: {precision(t_mtchs_total, t_false_total)}
+    Absolute Techniques: {precision(t_abs_mtchs_total, t_abs_false_total)}
+{'-'*50}
 Time Taken: {round((time.time() - start_time)/60, 2)} minuites
 {'='*50}"""
     print_log(final_log)
@@ -234,7 +214,7 @@ def print_log(str):
 def main():
     large_model = "gpt-oss:latest"
     fast_model = "gpt-oss:latest"
-    test_batch_clf(num_tests=25, checkpoint=0)
+    test_clf(mode=ClfMode.BATCH,num_tests=5, checkpoint=0)
 
 if __name__ == "__main__":
     main()
